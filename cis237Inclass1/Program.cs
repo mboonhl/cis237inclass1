ebd33a8101ad15bfe532769b499270399b4989dc
Morgan Boon <mboonhl@gmail.com>
2016-09-13 17:06:37 -0400

    UserInterface was created.

--- cis237Inclass1/Program.cs
+++ cis237Inclass1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,33 @@ namespace cis237Inclass1
     {
         static void Main(string[] args)
         {
+            Employee myEmployee = new Employee();
+            myEmployee.FirstName = "Morgan";
+            myEmployee.LastName = "Boon";
+            myEmployee.WeeklySalary = 298763.02m;
+
+            Console.WriteLine(myEmployee.FirstName);
+            Console.WriteLine(myEmployee);
+
+            Employee[] employees = new Employee[10];
+
+            employees[0] = new Employee("James", "blah", 45667.01m);
+            employees[1] = new Employee("Jam", "blam", 45660000007.01m);
+            employees[2] = new Employee("Js", "b", 67.01m);
+            employees[3] = new Employee("Mes", "cah", 0.01m);
+            employees[4] = new Employee("Sam", "lah", 7.01m);
+            employees[5] = new Employee("Jack", "ah", 4563267.01m);
+            employees[6] = new Employee("Ram", "blh", 4524667.01m);
+
+            foreach (Employee employee in employees)
+            {
+                if (employee != null)
+                {
+                    Console.WriteLine(employee.ToString() + " " + employee.YearlySalary());
+
+                }
+
+            }
         }
     }
 }
---
+++ cis237Inclass1/UserInterface.cs
@@ -0,0 +1,21 @@
+﻿namespace cis237Inclass1
+{
+    public class UserInterface
+    {
+
+        public void printMenu()
+        {
+            Console.Writeline("What would you like to do?");
+            Console.Writeline("1. Print List");
+            Console.Writeline("2. Exit");
+
+
+
+
+        }
+
+
+
+
+    }
+}
